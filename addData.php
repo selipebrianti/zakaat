038a084d86b6d75cc393a8a0a9210c5e9b51c81f
selipebrianti <selyfebrianti688@gmail.com>
2024-07-20 03:56:22 -0700

    Add files via upload

---
+++ addData.php
@@ -0,0 +1,189 @@
+<?php
+session_start();
+// Jika tidak bisa login maka balik ke login.php
+// jika masuk ke halaman ini melalui url, maka langsung menuju halaman login
+if (!isset($_SESSION['login'])) {
+    header('location:login.php');
+    exit;
+}
+
+// Memanggil atau membutuhkan file function.php
+require 'function.php';
+
+// Jika fungsi tambah lebih dari 0/data tersimpan, maka munculkan alert dibawah
+if (isset($_POST['simpan'])) {
+    if (tambah($_POST) > 0) {
+        echo "<script>
+                alert('Data zakat berhasil ditambahkan!');
+                document.location.href = 'index.php';
+            </script>";
+    } else {
+        // Jika fungsi tambah dari 0/data tidak tersimpan, maka munculkan alert dibawah
+        echo "<script>
+                alert('Data zakat gagal ditambahkan!');
+            </script>";
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+     <!-- Bootstrap -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+     <!-- Bootstrap Icons -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+     <!-- Font Google -->
+     <link rel="preconnect" href="https://fonts.gstatic.com">
+     <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+     <!-- animasi Css Aos -->
+     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+     <!-- My CSS -->
+     <link rel="stylesheet" href="css/style.css">
+
+     <title>Tambah Data</title>
+</head>
+
+<body background="img/bg/bck.png">
+     <!-- Navbar -->
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-uppercase">
+          <div class="container">
+               <a class="navbar-brand" href="index.php">Sistem Admin Data Zakat</a>
+               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                         <li class="nav-item">
+                              <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                         </li>
+                         <li class="nav-item">
+                              <a class="nav-link" href="#about">About</a>
+                         </li>
+                         <li class="nav-item">
+                              <a class="nav-link" href="logout.php">Logout</a>
+                         </li>
+                    </ul>
+               </div>
+          </div>
+     </nav>
+     <!-- Close Navbar -->
+
+     <!-- Container -->
+     <div class="container">
+          <div class="row my-2">
+               <div class="col-md text-light">
+                    <h3 class="fw-bold text-uppercase Tambah_data"></h3>
+               </div>
+               <hr>
+          </div>
+          <div class="row my-2 text-light">
+               <div class="col-md">
+                    <form action="" method="post" enctype="multipart/form-data">
+                         <div class="mb-3">
+                              <label for="nik" class="form-label">NIK</label>
+                              <input type="number" class="form-control w-50" id="nik" placeholder="Masukkan NIK" min="1"
+                                   name="nik" autocomplete="off" required>
+                         </div>
+                         <div class="mb-3">
+                              <label for="nama" class="form-label">Nama</label>
+                              <input type="text" class="form-control form-control-md w-50" id="nama"
+                                   placeholder="Masukkan Nama" name="nama" autocomplete="off" required>
+                         </div>
+                         <div class="mb-3">
+                              <label for="jenis" class="form-label">Jenis</label>
+                              <input type="text" class="form-control w-50" id="jenis" name="jenis" required>
+                         </div>
+                         <div class="mb-3">
+                              <label for="jumlah" class="form-label">Jumlah</label>
+                              <input type="text" class="form-control w-50" id="jumlah"
+                                   placeholder="Masukkan Jumlah" name="jumlah" autocomplete="off" required>
+                         </div>
+                         <div class="mb-3">
+                              <label>Jenis Kelamin</label>
+                              <div class="form-check">
+                                   <input class="form-check-input" type="radio" name="jekel" id="Laki - Laki"
+                                        value="Laki - Laki">
+                                   <label class="form-check-label" for="Laki - Laki">Laki - Laki</label>
+                              </div>
+                              <div class="form-check">
+                                   <input class="form-check-input" type="radio" name="jekel" id="Perempuan"
+                                        value="Perempuan">
+                                   <label class="form-check-label" for="Perempuan">Perempuan</label>
+                              </div>
+                         </div>
+                         </div>
+                         <div class="mb-3">
+                              <label for="gambar" class="form-label">Gambar</label>
+                              <input class="form-control form-control-sm w-50" id="gambar" name="gambar" type="file">
+                         </div>
+                         <div class="mb-3">
+                              <label for="alamat" class="form-label">Alamat</label>
+                              <textarea class="form-control w-50" id="alamat" rows="5" name="alamat"
+                                   placeholder="Masukkan Alamat" autocomplete="off" required></textarea>
+                         </div>
+                         <hr>
+                         <a href="index.php" class="btn btn-secondary">Kembali</a>
+                         <button type="submit" class="btn btn-primary" name="simpan">Simpan</button>
+                    </form>
+               </div>
+          </div>
+     </div>
+     <!-- Close Container -->
+
+
+
+     <!-- Footer -->
+     <div class="container-fluid">
+          <div class="row bg-dark text-white text-center">
+               <div class="col my-2" id="about">
+                    <br><br><br>
+                    <h4 class="fw-bold text-uppercase">About</h4>
+
+                    <p>
+                              Pembuat:
+                        <br> 1. Irfan </br>
+                        <br> 2. Indah </br>
+                        <br> 3. Laila </br>
+                        <br> 4. Jaffar </br>
+                        <br> 5. Ridwan </br>
+                        <br> 6. Saima </br>
+                    </p>
+               </div>
+          </div>
+     </div>
+     <!-- Close Footer -->
+
+     <!-- Bootstrap -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+     </script>
+
+     <!-- animasi  gsap-->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"> </script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/TextPlugin.min.js"></script>
+     <script>
+     gsap.registerPlugin(TextPlugin);
+     gsap.to('.Tambah_data', {
+          duration: 2,
+          delay: 1,
+          text: '<i class="bi bi-person-plus-fill"></i>Tambah Data Zakat'
+     })
+     gsap.from('.navbar', {
+          duration: 1,
+          y: '-100%',
+          opacity: 0,
+          ease: 'bounce',
+     })
+     </script>
+</body>
+
+</html>
